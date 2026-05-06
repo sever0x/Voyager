9a4e1acc81eca65183318152e1a34abca3fa0c02
Yaroslava Nechaieva <shdwrazedev@gmail.com>
2026-05-07 00:00:55 +0200

    Update .gitignore to exclude lock files; fix TypeScript type issues in CollectBlock.ts; pin langchain version in requirements.txt

--- voyager/env/mineflayer/mineflayer-collectblock/src/CollectBlock.ts
+++ voyager/env/mineflayer/mineflayer-collectblock/src/CollectBlock.ts
@@ -68,11 +68,11 @@ async function collectAll(
                             // @ts-ignore
                         } else if (err.name === "NoItem") {
                             const properties =
-                                bot.registry.blocksByName[closest.name];
+                                bot.registry.blocksByName[closest.name!];
                             const leastTool = Object.keys(
-                                properties.harvestTools
+                                properties.harvestTools ?? {}
                             )[0];
-                            const item = bot.registry.items[leastTool];
+                            const item = bot.registry.items[parseInt(leastTool)];
                             bot.chat(
                                 `I need at least a ${item.name} to mine ${closest.name}!  Skip it!`
                             );
